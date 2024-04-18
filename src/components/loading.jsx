0a13369cb92a0a2d904a85a7a974a49120baac5b
asdacosta <asdacosta18@gmail.com>
2024-04-18 01:44:09 +0000

    Display loading with text and gif

---
+++ src/components/loading.jsx
@@ -0,0 +1,15 @@
+function Loading() {
+  return (
+    <div className="load">
+      <iframe
+        src="https://giphy.com/embed/VtR7ehaGlRyPRjmEzG"
+        frameBorder="0"
+        className="giphy-embed"
+        allowFullScreen
+      ></iframe>
+      <p>Initializing...</p>
+    </div>
+  );
+}
+
+export { Loading };
