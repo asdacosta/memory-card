3caa43aaa8c9fefe9f2d86cde7b2720fdf220a93
asdacosta <asdacosta18@gmail.com>
2024-04-19 11:12:32 +0000

    Run load page before displaying boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./stylesheets/reset.css";
 import "./stylesheets/App.css";
 import { Loading } from "./components/loading.jsx";
+import { Boilerplate } from "./components/boilerplate.jsx";
 
 function App() {
-  return (
-    <>
-      <Loading></Loading>
-    </>
-  );
+  const [load, setLoad] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoad(false);
+    }, 2000);
+  }, []);
+
+  return <>{load ? <Loading /> : <Boilerplate />}</>;
 }
 
 export default App;
