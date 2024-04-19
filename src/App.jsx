82db759c6cf8ec46922cbd91e276aefe5128af9c
asdacosta <asdacosta18@gmail.com>
2024-04-19 11:20:08 +0000

    Adjust import names that changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import "./stylesheets/reset.css";
 import "./stylesheets/App.css";
-import { Loading } from "./components/loading.jsx";
-import { Boilerplate } from "./components/boilerplate.jsx";
+import { Loading } from "./components/Loading.jsx";
+import { Boilerplate } from "./components/Boilerplate.jsx";
 
 function App() {
   const [load, setLoad] = useState(true);
