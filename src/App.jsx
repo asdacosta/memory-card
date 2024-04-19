99952044b65cfb7fb99144a0b99f45919c873c74
asdacosta <asdacosta18@gmail.com>
2024-04-19 19:02:22 +0000

    Fetch urls during loading

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import "./stylesheets/reset.css";
 import "./stylesheets/App.css";
 import { Loading } from "./components/Loading.jsx";
 import { Boilerplate } from "./components/Boilerplate.jsx";
+import { getImgUrls } from "./components/FetchImgs.jsx";
 
 function App() {
   const [load, setLoad] = useState(true);
+  const [imgUrls, setImgUrls] = useState(null);
 
   useEffect(() => {
+    setImgUrls(getImgUrls());
     setTimeout(() => {
       setLoad(false);
     }, 2000);
   }, []);
 
-  return <>{load ? <Loading /> : <Boilerplate />}</>;
+  return <>{load ? <Loading /> : <Boilerplate urls={imgUrls} />}</>;
 }
 
 export default App;
